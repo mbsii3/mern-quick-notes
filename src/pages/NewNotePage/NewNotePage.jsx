83d64925e09b6fd0b100dade7bea9e6b5c6faeaa
mbsii3 <mshortjr3@gmail.com>
2023-04-06 00:44:13 -0500

    associate handle functions with note form

--- src/pages/NewNotePage/NewNotePage.jsx
+++ src/pages/NewNotePage/NewNotePage.jsx
@@ -1,3 +1,27 @@
+import { useState } from "react";
+
 export default function NewNotePage() {
-    return <h1>NewNotePage</h1>
+
+    const [note, setNote] = useState('');
+
+    function handleChange(evt) {
+        setNote(evt.target.value);
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        alert('clicked')
+    }
+
+    return (
+        <>
+        <h1>NewNotePage</h1>
+        <form action="">
+            <label htmlFor="text">Text:
+                <input type="text" name="text" id="text" onChange={handleChange} />
+            </label>
+            <input type="submit" value="Submit" onClick={handleSubmit} />
+        </form>
+        </>
+    );
 }
